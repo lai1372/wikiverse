209f7660c23b5706463c230d85dda6e49ed6c23d
Laila Case <lai_case@hotmail.com>
2023-11-19 14:21:38 +0000

    add delete functionality for articles including success message

--- public/react/pages/Article.js
+++ public/react/pages/Article.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 
 export default function Article() {
   const [article, setArticle] = useState({});
+  const [deleteSuccess, setDeleteSuccess] = useState(false);
   const { slug } = useParams();
   const navigate = useNavigate();
 
@@ -23,6 +24,14 @@ export default function Article() {
     setArticle(data);
   }
 
+  async function handleDelete() {
+    const response = await fetch(`${apiURL}/wiki/${slug}`, {
+      method: "DELETE",
+    });
+    const data = await response.json();
+    setDeleteSuccess(true);
+  }
+
   useEffect(() => {
     getArticle();
   }, []);
@@ -30,19 +39,30 @@ export default function Article() {
   function handleClick() {
     navigate("/");
   }
+
   return (
     <>
-      {article.title && <h1>{article.title}</h1>}
-      {article.author && <p>{article.author.name}</p>}
-      <br></br>
-      <p>Tag(s):</p>
-      {article.tags && article.tags.map((tag, i) => <p key={i}>{tag.name}</p>)}
-      <br></br>
-      {article.createdAt && (
-        <p>Created at: {formatDate.toLocaleDateString(undefined, options)}</p>
+      {!deleteSuccess ? (
+        <>
+          {article.title && <h1>{article.title}</h1>}
+          {article.author && <p>{article.author.name}</p>}
+          <br></br>
+          <p>Tag(s):</p>
+          {article.tags &&
+            article.tags.map((tag, i) => <p key={i}>{tag.name}</p>)}
+          <br></br>
+          {article.createdAt && (
+            <p>
+              Created at: {formatDate.toLocaleDateString(undefined, options)}
+            </p>
+          )}
+          {article.content && <p>{article.content}</p>}
+          <button onClick={handleClick}>Back to main</button>
+          <button onClick={handleDelete}>Delete article</button>
+        </>
+      ) : (
+        <p>Post deleted</p>
       )}
-      {article.content && <p>{article.content}</p>}
-      <button onClick={handleClick}>Back to main</button>
     </>
   );
 }
